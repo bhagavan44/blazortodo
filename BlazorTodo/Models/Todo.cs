bafe61d5183dddb50310457bc24b3f480d56d88c
Bhagavan Reddy <bhagavan@HOME.local>
2019-08-18 20:20:10 +0530

    Add project files.

---
+++ BlazorTodo/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorTodo.Models
+{
+    public class Todo
+    {
+        public Todo()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ BlazorTodo/Services/TodoService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorTodo.Models;
+using RestSharp;
+
+namespace BlazorTodo.Services
+{
+    public class TodoService
+    {
+        private readonly IRestClient restClient;
+
+        public TodoService()
+        {
+            restClient = new RestClient
+            {
+                BaseUrl = new System.Uri("https://jsonplaceholder.typicode.com/todos"),
+                Timeout = 2000
+            };
+        }
+
+        public async Task<List<Todo>> GetAsync()
+        {
+            RestRequest restRequest = new RestRequest("?_limit=5");
+
+            var response = await restClient.ExecuteGetTaskAsync<List<Todo>>(restRequest);
+
+            return response.Data;
+        }
+
+        public async Task<Todo> ToggleCompletedAsync(Todo todo)
+        {
+            RestRequest restRequest = new RestRequest("/" + todo.Id);
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddJsonBody(todo);
+
+            var response = await restClient.PutAsync<Todo>(restRequest);
+
+            return response;
+        }
+
+        public async Task<Todo> DeleteAsync(Todo todo)
+        {
+            RestRequest restRequest = new RestRequest("/" + todo.Id);
+            restRequest.AddHeader("Content-Type", "application/json");
+
+            var response = await restClient.DeleteAsync<Todo>(restRequest);
+
+            return response;
+        }
+
+        public async Task<Todo> AddAsync(Todo todo)
+        {
+            RestRequest restRequest = new RestRequest();
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddJsonBody(todo);
+
+            var response = await restClient.PostAsync<Todo>(restRequest);
+
+            return response;
+        }
+    }
+}
